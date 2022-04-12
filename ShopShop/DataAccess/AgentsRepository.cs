807f3e2b908a8830aba6fac90cd161f64b346bcf
Halie Dyer <halie.l.dyer@gmail.com>
2022-04-11 19:35:37 -0500

    getAllAgents, getAgentById, AddAgent, UpdateAgent, DeleteAgent

--- ShopShop/DataAccess/AgentsRepository.cs
+++ ShopShop/DataAccess/AgentsRepository.cs
@@ -20,6 +20,7 @@ namespace ShopShop.DataAccess
             }
         }
 
+        //Get all agents
         public List<Agent> GetAllAgents()
         {
             using (SqlConnection conn = Connection)
@@ -44,12 +45,128 @@ namespace ShopShop.DataAccess
                             Name = reader.GetString(reader.GetOrdinal("Name")),
                             Phone = reader.GetString(reader.GetOrdinal("Phone")),
                             Email = reader.GetString(reader.GetOrdinal("Email")),
-                            ImgURL = reader.GetString(reader.GetOrdinal("ImgURL")),
+                            ImgURL = reader.GetString(reader.GetOrdinal("ImgURL"))
                         };
                         agents.Add(agent);
                     }
                     reader.Close();
                     return agents;
+                }
+            }
+        }
+
+        //Get an agent by Id
+        public Agent GetAgentById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id,
+	                                           [Name],
+	                                           Phone,
+	                                           Email, 
+	                                           ImgURL
+                                        FROM Agent
+										WHERE Id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        Agent agent = new Agent()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Phone = reader.GetString(reader.GetOrdinal("Phone")),
+                            Email = reader.GetString(reader.GetOrdinal("Email")),
+                            ImgURL = reader.GetString(reader.GetOrdinal("ImgURL"))
+                        };
+                        reader.Close();
+                        return agent;
+                    }
+                    else
+                    {
+                        reader.Close();
+                        return null;
+                    }
+                }
+            }
+        }
+
+        //Add an agent
+
+        public void AddAgent(Agent agent)
+        {
+            using(SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Agent ([Name],
+					                                        Email,
+					                                        Phone,
+					                                        ImgURL)
+                                        OUTPUT INSERTED.Id
+                                        VALUES (@name, @email, @phone, @phone)";
+
+                    cmd.Parameters.AddWithValue("@name", agent.Name);
+                    cmd.Parameters.AddWithValue("@email", agent.Email);
+                    cmd.Parameters.AddWithValue("@phone", agent.Phone);
+                    cmd.Parameters.AddWithValue("@ImgUrl", agent.ImgURL);
+
+                    int id = (int)cmd.ExecuteScalar();
+
+                    agent.Id = id;
+                }
+            }
+        }
+
+        //Update an agent
+
+        public void UpdateAgent(Agent agent)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"UPDATE Owner
+                                        SET [Name] = @name, 
+                                            Email = @email, 
+                                            Phone = @phone
+                                        WHERE Id = @id";
+
+                    cmd.Parameters.AddWithValue("@name", agent.Name);
+                    cmd.Parameters.AddWithValue("@email", agent.Email);
+                    cmd.Parameters.AddWithValue("@phone", agent.Phone);
+                    cmd.Parameters.AddWithValue("@id", agent.Id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        //Delete an agent
+
+        public void DeleteAgent(int agentId)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"DELETE FROM Agent
+                                        WHERE Id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", agentId);
+
+                    cmd.ExecuteNonQuery();
                 }
             }
         }
