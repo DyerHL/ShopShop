c033f37da87d0c1b18168565acafa8a3c6773bb1
Halie Dyer <halie.l.dyer@gmail.com>
2022-04-16 12:18:16 -0500

    navbar layout completed, styled to match mockup

---
+++ shopshopclient/src/Initialize/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../Components/Navbar';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function Initialize() {
+  return (
+    <div>
+        <Navbar />
+        <p>"Initialize file is working"</p>
+        {/*Routing*/}
+        {/*Footer*/}
+    </div>
+  );
+}
+
+export default Initialize;
