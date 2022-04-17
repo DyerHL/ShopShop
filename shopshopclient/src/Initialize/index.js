49b2717d533940c6c9ffcd8d88f31b9eef755f52
Halie Dyer <halie.l.dyer@gmail.com>
2022-04-17 11:42:56 -0500

    created about view

--- shopshopclient/src/Initialize/index.js
+++ shopshopclient/src/Initialize/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
 import 'bootstrap/dist/css/bootstrap.css';
+import About from '../Views/About';
 
 function Initialize() {
   return (
     <div>
         <Navbar />
         <p>"Initialize file is working"</p>
+        <About />
         {/*Routing*/}
         {/*Footer*/}
     </div>
---
+++ shopshopclient/src/Views/About.js
@@ -0,0 +1,23 @@
+ import React from "react";
+ import nashSkyline from '../Assets/nashSkyline';
+ 
+
+ export default function About() {
+     return (
+         <div>
+             <div>
+                 <img src={nashSkyline} className="about-skyline"/>
+                 img
+             </div>
+             <div>
+                 title
+             </div>
+             <div>
+                 break
+             </div>
+             <div>
+                 description
+             </div>
+         </div>
+     )
+ }
