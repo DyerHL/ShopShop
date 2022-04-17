c1b0fb867d21a23b1fb48d4b829963bd0853854f
pmpurcell93 <maddenpurcell@gmail.com>
2022-04-16 20:08:40 -0600

    Worked on listing view

---
+++ shopshopclient/src/views/ListingsView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from "../Navbar";
+
+export default function ListingsView() {
+  return (
+    <div>
+        <Navbar></Navbar>
+        <div id="ListingBanner"></div>
+        <div></div>
+    </div>
+  )
+}
