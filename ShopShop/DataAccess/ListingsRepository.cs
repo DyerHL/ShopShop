73214658655e1d1d8b6c0604ea11e6f17188671c
pmpurcell93 <maddenpurcell@gmail.com>
2022-04-19 19:33:12 -0600

    Created getListingsByAgentId method and added to Interface and Controller.

--- ShopShop/DataAccess/ListingsRepository.cs
+++ ShopShop/DataAccess/ListingsRepository.cs
@@ -126,6 +126,57 @@ namespace ShopShop.DataAccess
             }
         }
 
+        public List<Listing> GetListingsByAgentId(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                      SELECT
+                                      Id,
+                                      Address,
+                                      SquareFoot,
+                                      Rent,
+                                      Details,
+                                      YearBuilt,
+                                      City,
+                                      ImgUrl,
+                                      AgentId,
+                                      UserId
+                                      FROM Listing WHERE AgentId = @id
+                                      ";
+
+                    cmd.Parameters.AddWithValue("id", id);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Listing> listings = new List<Listing>();
+                    while (reader.Read())
+                    {
+                        Listing listing = new Listing
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Address = reader.GetString(reader.GetOrdinal("Address")),
+                            SquareFoot = reader.GetInt32(reader.GetOrdinal("SquareFoot")),
+                            Rent = reader.GetInt32(reader.GetOrdinal("Rent")),
+                            Details = reader.GetString(reader.GetOrdinal("Details")),
+                            YearBuilt = reader.GetInt32(reader.GetOrdinal("YearBuilt")),
+                            City = reader.GetString(reader.GetOrdinal("City")),
+                            ImgUrl = reader.GetString(reader.GetOrdinal("ImgUrl")),
+                            AgentId = reader.GetInt32(reader.GetOrdinal("AgentId")),
+                            UserId = reader.GetInt32(reader.GetOrdinal("UserId"))
+                        };
+                        listings.Add(listing);
+
+                    }
+                        reader.Close();
+                        return listings;
+                }
+            }
+        }
+
         public void UpdateListing(Listing listing)
         {
             using (SqlConnection conn = Connection)
