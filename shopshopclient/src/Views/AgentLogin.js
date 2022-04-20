13b69b9e82d55276656202de5e479d0840b8f97a
Derek Baylor <dbaylor25@gmail.com>
2022-04-20 18:27:12 -0500

    agentLogin, agentHome, agentProfile views

--- shopshopclient/src/Views/AgentLogin.js
+++ shopshopclient/src/Views/AgentLogin.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 export default function AgentLogin() {
   return (
-      <div className='agent-login-body'>
+      <div className='body-div  agent-login-body'>
         <div className='page-title'>Agent Login</div>
-        <div className='form-divs'>
+        <hr />
+        <div className='div-1 form-divs'>
             <div className='create-account-form'> Create Account</div>
             <div className='login-form'>Login</div>
         </div>
