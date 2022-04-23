148a22f1467a81ed113ffe8b7aaf189f7807f4f5
pmpurcell93 <maddenpurcell@gmail.com>
2022-04-23 09:14:37 -0600

    Added listing card component to listing view

--- shopshopclient/src/views/ListingsView.js
+++ shopshopclient/src/views/ListingsView.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Footer from "../Components/Footer";
+import ListingCard from "../Components/ListingCard";
 import Navbar from "../Components/Navbar";
 
 
@@ -14,12 +15,7 @@ export default function ListingsView() {
       <h2>Listings</h2>
       <div class="div-body">
         <img src="https://wgfuljukrcejubfajlyl.supabase.co/storage/v1/object/sign/images/ListingViewImage.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZXMvTGlzdGluZ1ZpZXdJbWFnZS5qcGciLCJpYXQiOjE2NTA0OTc5MDIsImV4cCI6MTk2NTg1NzkwMn0.g7BVed0bJPnszxGLWGytOMCxISDAyXRalTUrrQa_DdU" alt="listing-img" />
-        <h2>Location Listing Location</h2>
-        <h2>Price Listing Price</h2>
-        <h2>Square Footage Listing SqFt</h2>
-        <h2>Year Built Listing Year</h2>
-        <h2>Description Listing Desc</h2>
-        <h2>Agent Listing Agent Name</h2>
+        <ListingCard></ListingCard>
       </div>
     </div>
   );
