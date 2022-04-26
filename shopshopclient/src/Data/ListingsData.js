6ee3f0bb1a59e269d2f4903041a797297321d24c
pmpurcell93 <maddenpurcell@gmail.com>
2022-04-25 18:55:02 -0600

    Attempt to fix polyfill issue by npm install react-rewired

--- shopshopclient/src/Data/ListingsData.js
+++ shopshopclient/src/Data/ListingsData.js
@@ -1,1 +1,12 @@
-// Listing API Calls Go Here //
+import axios from "axios";
+
+const dbUrl = "shopshopserver.database.windows.net/api";
+
+const getListings = () => new Promise((resolve, reject) => {
+    axios
+        .get(`${dbUrl}/listings.json`)
+        .then((response) => resolve(Object.values(response.data)))
+        .catch(reject);
+});
+
+export default getListings;
