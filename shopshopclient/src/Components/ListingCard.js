cbc9a94ede8128f21b64de0e1c955fe27d47f0a9
pmpurcell93 <maddenpurcell@gmail.com>
2022-04-27 19:52:19 -0600

    Working on SingleListing view

--- shopshopclient/src/Components/ListingCard.js
+++ shopshopclient/src/Components/ListingCard.js
@@ -1,15 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import PropTypes from "prop-types";
 
 export default function ListingCard({listing}) {
   return (
-    <div>
-        <h2>{listing.address}</h2>
-        <h2>{listing.city}</h2>
-        <h2>{listing.details}</h2>
-        <h2>${listing.rent}</h2>
-        <h2>{listing.squareFoot}</h2>
-        <h2>{listing.yearBuilt}</h2>
+    <div className='listing-card'>
+      <img class="img" src={listing.imgUrl} alt="img" />
+      <div className='listing-card-body'>
+        <h6>{listing.address}</h6>
+        <h6>{listing.city}</h6>
+        <h6>{listing.details}</h6>
+        <h6>${listing.rent}</h6>
+        <h6>Square Feet: {listing.squareFoot}</h6>
+        <h6>Year Built: {listing.yearBuilt}</h6>
+        <Link
+              type="button"
+              className="btn btn-primary"
+              to={`/Listings/Listings/${listing.id}`}
+            >
+              Details
+        </Link>
+      </div>
     </div>
   )
 }
---
+++ shopshopclient/src/Views/SingleListingView.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { getListingsById } from '../Data/ListingsData';
+
+export default function SingleListingView() {
+    const [listing, setListing] = useState({});
+    const { id } = useParams();
+
+    useEffect(() => {
+        let isMounted = true;
+        if (isMounted) {
+          getListingsById(id).then(setListing);
+        }
+        return () => {
+          isMounted = false;
+        };
+      }, [id]);
+
+      console.warn(listing);
+
+  return (
+    <div>{listing.city}</div>
+  )
+}
